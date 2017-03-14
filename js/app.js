d7847f143eb69bd5af313056aab30bb90807af6e
Calvin Ellington <calvinje@gmail.com>
2017-03-14 15:14:47 -0400

    Added location data, implemented basic app.js structure.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,50 @@
+// --- Declaring Strict Mode.
 "use strict";
 
-// Initial Location Data.
+// --- Initial Location Data.
 var InitLocations = [
-  
-]
+  {
+    name: 'Doobys',
+    lat: 39.298811,
+    long: -76.615898
+  },
+  {
+    name: 'Park Cafe',
+    lat: 39.308889,
+    long: -76.625599
+  },
+  {
+    name: 'Mt. Royal Tavern',
+    lat: 39.306803,
+    long: -76.620491
+  },
+  {
+    name: 'On The Hill',
+    lat: 39.308437,
+    long: -76.623762
+  },
+  {
+    name: 'The Bun Shop',
+    lat: 39.300623,
+    long: -76.619562
+  },
+  {
+    name: 'Station North Arts Cafe',
+    lat: 39.310422,
+    long: -76.616725
+  }
+
+];
+
+// --- Global Variables.
+var ClientID;
+var ClientSecret;
+var map;
+
+// --- Handler.
+
+// --- View.
+
+// --- Initalize Application.
+
+// --- Error Handler.
